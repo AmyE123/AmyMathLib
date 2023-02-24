c7f55fe4b14996d7691eb45a55f495e7c71b3f18
AmyE123 <amy.elliott2002@yahoo.co.uk>
2023-02-24 23:35:49 +0000

    Fixed the EulerAngles

--- Assets/_Workshops/Workshop03/EulerAngles.cs
+++ Assets/_Workshops/Workshop03/EulerAngles.cs
@@ -10,6 +10,7 @@ public class EulerAngles : MonoBehaviour
     private AVector3 _eulerAngle = new AVector3(0, 0, 0);
     private AVector3 _direction = new AVector3(0, 0, 0);
     private AVector3 _crossProduct = new AVector3(0, 0, 0);
+    private AVector3 _constantUpward = new AVector3(0, 1, 0);
 
     public const float SPEED = 0.02f;
 
@@ -25,33 +26,36 @@ public class EulerAngles : MonoBehaviour
         if (Input.GetKey(KeyCode.LeftArrow))
         {
             // TODO: Move target yaw (left/right)
-            _eulerAngle.y += 0.05f;
+            _eulerAngle.y += 0.01f;
         }
         if (Input.GetKey(KeyCode.RightArrow))
         {
             // TODO: Move target yaw (left/right)
-            _eulerAngle.y -= 0.05f;
+            _eulerAngle.y -= 0.01f;
         }
 
         if (Input.GetKey(KeyCode.UpArrow))
         {
             // TODO: Move target pitch (up/down)
-            _eulerAngle.x += 0.05f;
+            _eulerAngle.x += 0.01f;
         }
         if (Input.GetKey(KeyCode.DownArrow))
         {
             // TODO: Move target pitch (up/down)
-            _eulerAngle.x -= 0.05f;
+            _eulerAngle.x -= 0.01f;
         }
 
         // convert euler to direction, gets forward vector
         _direction = AMaths.EulerAngleToDirection(_eulerAngle);
 
         // cross product gets right vector
-        _crossProduct = AMaths.VectorCrossProduct(_eulerAngle, _direction);
+        _crossProduct = AMaths.VectorCrossProduct(_constantUpward, _direction);
+
+        var _other = transform.position + (_direction.ToUnityVector3() * 10);
 
         //draw ray 
-        Debug.DrawLine(_target.transform.position, _crossProduct.ToUnityVector3(), Color.blue);
+        //Debug.DrawLine(_target.transform.position, _crossProduct.ToUnityVector3(), Color.blue);
+        Debug.DrawLine(_target.transform.position, _other, Color.blue);
     }
 
     private void TargetMovement()
