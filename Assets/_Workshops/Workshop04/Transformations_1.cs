90a4d8c2f8b998682602be51dc09f3b9bcba34ab
AmyE123 <amy.elliott2002@yahoo.co.uk>
2023-02-28 16:20:33 +0000

    - Imported TextMesh Pro
    - Got mesh manipulation working
    - Got lerping working

--- Assets/_Workshops/Workshop04/Transformations_1.cs
+++ Assets/_Workshops/Workshop04/Transformations_1.cs
@@ -7,85 +7,166 @@ using UnityEngine.UIElements;
 public class Transformations_1 : MonoBehaviour
 {
     #region MESH MANIPULATION TASK
+    [Header("Mesh Manipulation")]
     [SerializeField]
-    private GameObject _player;
+    private GameObject _translateMeshGO;
 
-    Vector3[] ModelSpaceVerticies;
+    [SerializeField]
+    private GameObject _rotateMeshGO;
+
+    [SerializeField]
+    private GameObject _scaleMeshGO;
+
+    [SerializeField]
+    private float _translation;
+
+    [SerializeField]
+    private float _angle;
+
+    [SerializeField]
+    private float _scale;
+
+    Vector3[] ModelSpaceVerticiesTranslation;
+    Vector3[] ModelSpaceVerticiesRotation;
+    Vector3[] ModelSpaceVerticiesScale;
     #endregion //MESH MANIPULATION TASK
 
     #region LERP TASK
+    [Header("Lerping")]
     [SerializeField]
-    private GameObject _evader;
+    private GameObject _gameObject;
 
     [SerializeField]
-    private GameObject _pursuer;
+    private float _speed = 5;
 
     [SerializeField]
-    private float _evaderSpeed = 10;
+    private Transform _pointA;
 
     [SerializeField]
-    private float _pursuerSpeed = 5;
+    private Transform _pointB;
 
-    [SerializeField]
-    private Transform _from;
-
-    [SerializeField]
-    private Transform _to;
+    private bool _canLerp;
 
     [SerializeField]
     private AMatrix4x4 _matrix;
-
-    private Vector3 _pursuerPos;
-    private Vector3 _evaderPos;
     #endregion //LERP TASK
 
     private void Start()
     {
         #region MESH MANIPULATION TASK
         //Mesh filter is a component that stores info about the current mesh
-        MeshFilter MF = GetComponent<MeshFilter>();
+        MeshFilter MF_trans = _translateMeshGO.GetComponent<MeshFilter>();
+        MeshFilter MF_rot = _rotateMeshGO.GetComponent<MeshFilter>();
+        MeshFilter MF_scale = _scaleMeshGO.GetComponent<MeshFilter>();
 
         //We get a copy of all the verticies
-        ModelSpaceVerticies = MF.sharedMesh.vertices;
-        #endregion //MESH MANIPULATION TASK
-        #region LERP TASK
-        _pursuerPos = _pursuer.transform.position;
-        _evaderPos= _evader.transform.position;
-        #endregion //LERP TASK
-    }
+        ModelSpaceVerticiesTranslation = MF_trans.sharedMesh.vertices;
+        ModelSpaceVerticiesRotation = MF_rot.sharedMesh.vertices;
+        ModelSpaceVerticiesScale = MF_scale.sharedMesh.vertices;
 
+        #endregion //MESH MANIPULATION TASK
+    }
 
     private void Update()
     {
         #region MESH MANIPULATION TASK
+
+        #region Translation
         //Define a new array with the correct size
-        Vector3[] TransformedVerticies = new Vector3[ModelSpaceVerticies.Length];
-
-        //Create our rotation matrix
-        AMatrix4x4 rotationMatrix = new AMatrix4x4(new AVector3(1, 0, 0), new AVector3(0, 1, 0), new AVector3(0, 0, 1), new AVector3(0, 0, 0));
-
-        //Create our scaling matrix (2x, y, z)
-        AMatrix4x4 scaleMatrix = new AMatrix4x4(new AVector3(1, 0, 0) * 2.0f, new AVector3(0, 1, 0), new AVector3(0, 0, 1), new AVector3(0, 0, 0));
+        Vector3[] TransformedVerticiesTranslation = new Vector3[ModelSpaceVerticiesTranslation.Length];
 
         //Create our translation matrix
-        AMatrix4x4 translationMatrix = new AMatrix4x4(new AVector3(1, 0, 0), new AVector3(0, 1, 0), new AVector3(0, 0, 1), new AVector3(transform.position.x, transform.position.y, transform.position.z));
+        AMatrix4x4 translationMatrix = new AMatrix4x4(new AVector3(1, 0, 0), new AVector3(0, 1, 0), new AVector3(0, 0, 1), new AVector3(0, 0, _translation));
 
         //Transform each individual vertex
-        for (int i = 0; i < TransformedVerticies.Length; i++)
+        for (int i = 0; i < TransformedVerticiesTranslation.Length; i++)
         {
-            var AModelSpaceVetex = AMaths.ToAVector(ModelSpaceVerticies[i]);
-            TransformedVerticies[i] = AMaths.ToUnityVector(rotationMatrix * AModelSpaceVetex);
+            var AModelSpaceVetex = AMaths.ToAVector(ModelSpaceVerticiesTranslation[i]);
+            AVector4 AModelSpaceVertex4 = new AVector4(AModelSpaceVetex.x, AModelSpaceVetex.y, AModelSpaceVetex.z, 1);
+            var Transformed4 = translationMatrix * AModelSpaceVertex4;
+            AVector3 Transformed3 = new AVector3(Transformed4.x, Transformed4.y, Transformed4.z);
+            TransformedVerticiesTranslation[i] = AMaths.ToUnityVector(Transformed3);
         }
 
         //Mesh filter is a component that stores info about the current mesh
-        MeshFilter MF = GetComponent<MeshFilter>();
+        MeshFilter MF_trans = _translateMeshGO.GetComponent<MeshFilter>();
 
         //Assign our new verticies
-        MF.mesh.vertices = TransformedVerticies;
+        MF_trans.mesh.vertices = TransformedVerticiesTranslation;
 
         //These final steps are cometimes necessary to make the mesh look correct
-        MF.mesh.RecalculateNormals();
-        MF.mesh.RecalculateBounds();
+        MF_trans.mesh.RecalculateNormals();
+        MF_trans.mesh.RecalculateBounds();
+        #endregion //Translation
+
+        #region Rotation
+        //Define a new array with the correct size
+        Vector3[] TransformedVerticiesRotation = new Vector3[ModelSpaceVerticiesRotation.Length];
+
+        //Create our rotation matrix, this time rotating around the Roll, Pitch and Yaw in that order
+        AMatrix4x4 rollMatrix = new AMatrix4x4(
+            new AVector3(Mathf.Cos(_angle), Mathf.Sin(_angle), 0),
+            new AVector3(-Mathf.Sin(_angle), Mathf.Cos(_angle), 0),
+            new AVector3(0, 0, 1),
+            new AVector3(0, 0, 0));
+
+        AMatrix4x4 pitchMatrix = new AMatrix4x4(
+            new AVector3(1, 0, 0),
+            new AVector3(0, Mathf.Cos(_angle), Mathf.Sin(_angle)),
+            new AVector3(0, -Mathf.Sin(_angle), Mathf.Cos(_angle)),
+            new AVector3(0, 0, 0));
+
+        AMatrix4x4 yawMatrix = new AMatrix4x4(
+            new AVector3(Mathf.Cos(_angle), 0, -Mathf.Sin(_angle)),
+            new AVector3(0, 1, 0),
+            new AVector3(Mathf.Sin(_angle), 0, Mathf.Cos(_angle)),
+            new AVector3(0, 0, 0));
+
+        //Transform each individual vertex
+        for (int i = 0; i < TransformedVerticiesRotation.Length; i++)
+        {
+            AVector3 RolledVetrex = rollMatrix * AMaths.ToAVector(ModelSpaceVerticiesRotation[i]);
+            AVector3 PitchedVertex = pitchMatrix * RolledVetrex;
+            AVector3 YawedVertex = yawMatrix * PitchedVertex;
+
+            TransformedVerticiesRotation[i] = AMaths.ToUnityVector(YawedVertex);
+        }
+
+        //Mesh filter is a component that stores info about the current mesh
+        MeshFilter MF_rot = _rotateMeshGO.GetComponent<MeshFilter>();
+
+        //Assign our new verticies
+        MF_rot.mesh.vertices = TransformedVerticiesRotation;
+
+        //These final steps are cometimes necessary to make the mesh look correct
+        MF_rot.mesh.RecalculateNormals();
+        MF_rot.mesh.RecalculateBounds();
+        #endregion //Rotation
+
+        #region Scale
+        //Define a new array with the correct size
+        Vector3[] TransformedVerticiesScale = new Vector3[ModelSpaceVerticiesScale.Length];
+
+        //Create our scaling matrix (2x, y, z)
+        AMatrix4x4 scaleMatrix = new AMatrix4x4(new AVector3(1, 0, 0) * _scale, new AVector3(0, 1, 0), new AVector3(0, 0, 1), new AVector3(0, 0, 0));
+
+        //Transform each individual vertex
+        for (int i = 0; i < TransformedVerticiesScale.Length; i++)
+        {
+            var AModelSpaceVetex = AMaths.ToAVector(ModelSpaceVerticiesScale[i]);
+            TransformedVerticiesScale[i] = AMaths.ToUnityVector(scaleMatrix * AModelSpaceVetex);
+        }
+
+        //Mesh filter is a component that stores info about the current mesh
+        MeshFilter MF_scale = _scaleMeshGO.GetComponent<MeshFilter>();
+
+        //Assign our new verticies
+        MF_scale.mesh.vertices = TransformedVerticiesScale;
+
+        //These final steps are cometimes necessary to make the mesh look correct
+        MF_scale.mesh.RecalculateNormals();
+        MF_scale.mesh.RecalculateBounds();
+        #endregion //Scale
 
         //Vector3 F = transform.position - _player.transform.position;
         //AVector3 R = AMaths.VectorCrossProduct(AMaths.ToAVector(Vector3.up), AMaths.ToAVector(F));
@@ -95,44 +176,29 @@ public class Transformations_1 : MonoBehaviour
 
         #endregion  //MESH MANIPULATION TASK
 
-        #region LERP TASK
+        #region LERP TASK      
         if (Input.GetKeyDown(KeyCode.L))
         {
-            EvaderMovement();
-            PursuerMovement();
+            _canLerp = !_canLerp;         
+        }
+
+        if (_canLerp)
+        {
+            Lerp(AMaths.ToAVector(_pointB.position));
+        }
+        if (!_canLerp)
+        {
+            Lerp(AMaths.ToAVector(_pointA.position));
         }
         #endregion //LERP TASK
     }
 
     #region LERP TASK
-    void PursuerMovement()
+    void Lerp(AVector3 target)
     {
-        var from = AMaths.ToAVector(_pursuerPos);
-        var to = AMaths.ToAVector(_evaderPos);
+        var lerp = AMaths.Lerp(AMaths.ToAVector(_gameObject.transform.position), target, Time.deltaTime * _speed);
 
-        var lerp = AMaths.Lerp(from, to, Time.deltaTime * _pursuerSpeed);
-
-        _pursuer.transform.position = lerp.ToUnityVector3();
-    }
-
-    void EvaderMovement()
-    {
-        if (Input.GetKey(KeyCode.W))
-        {
-            _evader.transform.Translate(Vector3.forward * Time.deltaTime * _evaderSpeed);
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            _evader.transform.Translate(-1 * Vector3.forward * Time.deltaTime * _evaderSpeed);
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            _evader.transform.Rotate(0, -1, 0);
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            _evader.transform.Rotate(0, 1, 0);
-        }
+        _gameObject.transform.position = lerp.ToUnityVector3();
     }
 #endregion //LERP TASK
 }
