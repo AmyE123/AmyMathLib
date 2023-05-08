c6b8861fba3465816343969fef568ccc40e359bd
AmyE123 <amy.elliott2002@yahoo.co.uk>
2023-05-08 23:17:07 +0100

    Fixed and cleanup of map rotation

--- Assets/Scripts/Level.cs
+++ Assets/Scripts/Level.cs
@@ -22,18 +22,19 @@ namespace BlockyRoad
         [SerializeField]
         private float _slerpSpeed;
 
-        private bool _rotating;
+        private bool _rotatingToSouth;
+        private bool _rotatingToNorth;
+
+        private AQuaternion _startRotNorth;
+        private AQuaternion _startRotSouth;
 
         private AQuaternion _finalRot;
-
-        private AQuaternion _startRot;
 
         private void Start()
         {
             _manager = FindObjectOfType<GameManager>();
 
             _finalRot = new AQuaternion(180 * Mathf.Deg2Rad, new AVector3(0, 0, 1)) * AQuaternion.ToAQuaternion(transform.rotation);
-            _startRot = AQuaternion.ToAQuaternion(transform.rotation);
         }
 
         void Update()
@@ -42,6 +43,9 @@ namespace BlockyRoad
             {
                 if (ActiveSide == Sides.North)
                 {
+                    _startRotNorth = AQuaternion.ToAQuaternion(transform.rotation);
+                    _finalRot = new AQuaternion(180 * Mathf.Deg2Rad, new AVector3(0, 0, 1)) * AQuaternion.ToAQuaternion(transform.rotation);
+
                     Debug.Log("[LEVEL] Turning North -> South");
                     ActiveSide = Sides.South;
 
@@ -49,13 +53,15 @@ namespace BlockyRoad
                     //transform.Rotate(180, 0, 0);
 
                     // -- New implementation --
-                    var ROT = new AQuaternion(180, new AVector3(0, 0, 180));
-
                     _slerpTime = 0;
-                    _rotating = true;                   
+                    _rotatingToSouth = true;
+                    _rotatingToNorth = false;
                 }
                 else
                 {
+                    _startRotSouth = AQuaternion.ToAQuaternion(transform.rotation);
+                    _finalRot = new AQuaternion(-180 * Mathf.Deg2Rad, new AVector3(0, 0, 1)) * AQuaternion.ToAQuaternion(transform.rotation);
+
                     Debug.Log("[LEVEL] Turning South -> North");
                     ActiveSide = Sides.North;
 
@@ -63,18 +69,19 @@ namespace BlockyRoad
                     //transform.Rotate(-180, 0, 0);
 
                     // -- New implementation --
-                    _rotating = false;
-                    var ROT = new AQuaternion(180, new AVector3(0, 0, 0));
+                    _slerpTime = 0;
+                    _rotatingToSouth = false;
+                    _rotatingToNorth = true;
                 }
             }
 
-            if (_rotating == true)
+            if (_rotatingToSouth)
             {
-                // -- Old implementation --
-                //transform.rotation = AQuaternion.ToUnityQuaternion(final);
-
-                // -- New implementation --
-                SlerpRotate(_startRot, _finalRot);                
+                SlerpRotate(_startRotNorth, _finalRot);
+            }
+            if (_rotatingToNorth)
+            {
+                SlerpRotate(_startRotSouth, _finalRot);
             }
         }
 
